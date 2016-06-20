a5561a851cb113cfb37e3e7bd8afda6a5e46bd60
mcidclan <m.cid.clan@gmail.com>
2016-06-20 10:13:13 +0200

    Add UIMessageBox files

---
+++ i3d-gui/UIMessageBox.cpp
@@ -0,0 +1,14 @@
+#include "UIMessageBox.hpp"
+
+UIMessageBox::UIMessageBox()
+{
+	this->message = NULL;
+	/*this->load(RES_UIMESSAGEBOX_MESH_FNAME);
+	this->setTextureId(utils::loadPng(tfname.c_str()));*/
+
+}
+
+UIMessageBox::~UIMessageBox()
+{
+}
+
